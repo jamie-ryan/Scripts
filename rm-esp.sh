48bda4f4fe46cb273b3d6bc9651d89d104fea399
jamie ryan <jamie.ryan.14@ucl.ac.uk>
2016-11-15 17:12:24 +0000

    eps rm

---
+++ rm-esp.sh
@@ -0,0 +1,62 @@
+#!/bin/tcsh
+
+cd /unsafe/jsr2/project2/20150311/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20140329/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20140207/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20140202/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20140107/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/201311071428/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/201311070339/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20131106/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20130708/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20130213/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20121023/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20120706/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/201207051134/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/201207050325/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20120704/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20120510/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20120509/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20110926/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20110730/HMI/v/
+rmall *.eps
+
+cd /unsafe/jsr2/project2/20110215/HMI/v/
+rmall *.eps
+
