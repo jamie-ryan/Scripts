b919cf5cb3540e668bfffe10dd3355dc153da671
jamie ryan <jamie.ryan.14@ucl.ac.uk>
2016-11-16 05:34:25 +0000

    dddd

---
+++ rename-dopp-context.sh
@@ -0,0 +1,62 @@
+#!/bin/tcsh
+
+mv /unsafe/jsr2/project2/20150311/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20150311_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20140329/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20140329_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20140207/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20140207_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20140202/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20140202_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20140107/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20140107_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/201311071428/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/201311071428_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/201311070339/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/201311070339_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20131106/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20131106_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20130708/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20130708_context_depth_image.eps    
+
+
+mv /unsafe/jsr2/project2/20130213/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20130213_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20121023/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20121023_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20120706/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20120706_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/201207051134/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/201207051134_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/201207050325/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/201207050325_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20120704/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20120704_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20120510/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20120510_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20120509/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20120509_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20110926/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20110926_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20110730/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20110730_context_depth_image.eps
+
+
+mv /unsafe/jsr2/project2/20110215/HMI/v/context_depth_image.eps /unsafe/jsr2/project2/depth_images/20110215_context_depth_image.eps
+
+
